cdbbd9f8a69f57720c1d2a0d2087d482532cf477
Cody Davis <Cody.1337@gmail.com>
2018-11-10 16:46:08 -0600

    initial upload of DiceBag 2.0 to repo. What was initially just practice with flex turned into a base I really liked and decided to convert it into DICEBAG2.0, thus why the project is so far along before initial commit. Working on the dice creation menu now. Just finished working on the diceDeck, diceCards, and nav bar. Been ensureing that these interfaces look good in mobile

---
+++ assets/javascript/index.js
@@ -0,0 +1,4 @@
+document.getElementById("buttonCreateDice").addEventListener('click', function(event){
+    console.log ('button: createDice touched');
+    document.getElementById("curtain").classList.toggle('hidden');
+});
